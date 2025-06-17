4f3dedf1936d957e05c77a54a4535ae64f6e8736
Satyabrata Saha <satyabratasaha20@gmail.com>
2025-06-17 19:44:02 +0530

    Add function to get date

--- src/main.go
+++ src/main.go
@@ -9,3 +9,8 @@ func GetCurrentTime() {
 	currentTime := time.Now()
 	fmt.Println("Current time is =", currentTime)
 }
+
+func GetCurrentDate() {
+	year, month, day := time.Now().Date()
+	fmt.Println("Current time is =", year, month, day)
+}
